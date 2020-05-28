4122736eebee8a96af9a3a428c2a0d4b64da5cbb
João Ventura <joaojonesventura@gmail.com>
2020-05-28 09:34:16 +0100

    Create empty Xcode project

---
+++ PyApp/PyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PyApp
+//
+//  Created by João Ventura on 28/05/2020.
+//  Copyright © 2020 Ventura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ reference.md
@@ -0,0 +1,8 @@
+# References for creating PyBridge-iOS
+
+This document contains the necessary steps to create an Xcode project from scratch and embedding the Python 3 interpreter.
+
+
+## Empty Xcode project
+
+Create an empty Xcode project (Single View Application). 
